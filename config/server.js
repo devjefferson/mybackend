f7cedaf26ee1fdd52c95068ba7d326867ee915e5
Jefferson Fonseca <drpcscan@gmail.com>
2021-10-26 14:33:34 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6b8d30b8bbc1e42ea9639673ca9b5fc0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '17f10033-e220-47fc-8506-4fdbaafeab19'
+};
