a8eec8da7d69ac2d11856895086cf18d00c30d9a
Jefferson Fonseca <drpcscan@gmail.com>
2021-12-01 11:25:38 -0200

    database render

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'strapi.cf7dn4yo95ff.sa-east-1.rds.amazonaws.com'),
+        host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi_bd'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi123'),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {
--- config/server.js
+++ config/server.js
@@ -1,10 +1,4 @@
 module.exports = ({ env }) => ({
-  url: env('MY_HEROKU_URL'),
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '6b8d30b8bbc1e42ea9639673ca9b5fc0'),
-    },
-  },
+  port: env.int('PORT', 1337)
 });
